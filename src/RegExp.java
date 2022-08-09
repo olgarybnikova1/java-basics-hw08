510ea9c7a453cfeb0e22bdd37b746889f4885287
Olga Rybnikova <olga.rybnikova777817@icloud.com>
2022-08-09 21:22:07 +0300

    getIpAddress_changes

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,16 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
 public class RegExp {
 
-    /**
-     * Get IP address from text.
-     *
-     * @param text text to parse
-     * @return String with IP address
-     */
-    public static String getIpAddress(String text) {
-        // TODO fill in code here for extracting IP address using Regular Expression
-        return "";
+  /**
+   * Get IP address from text.
+   *
+   * @param text text to parse
+   * @return String with IP address
+   */
+  public static String getIpAddress(String text) {
+    String IpAddress = "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.){3}(25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)";
+    Pattern pattern = Pattern.compile(IpAddress);
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      return matcher.group();
     }
+    return "";
+  }
 }
